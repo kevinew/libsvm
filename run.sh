bd6755e10b383b812d1289cf0da03fe153db4051
Wenkai Liu <kevinew.1221@gmail.com>
2014-05-02 21:59:39 -0700

    add run.sh script.

---
+++ run.sh
@@ -0,0 +1,9 @@
+./svm-train testdata/heart_scale testdata/heart_scale.model
+./svm-predict testdata/heart_scale testdata/heart_scale.model testdata/heart_scale.predicted
+
+#./svm-train testdata/sina_channel.train sina_channel.model
+#./svm-predict testdata/sina_channel.test sina_channel.model sina_channel.predicted
+
+
+
+#./svm-train ../../logistic_regression/data/train.txt model.txt
